6638bc915db424b6feafe72e547648e16642dbab
gkumar-pandey <gautamshekhar078@gmail.com>
2023-06-17 22:41:03 +0530

    style: make search input responsive

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -18,7 +18,7 @@ const SearchPage = () => {
   return (
     <div>
       <div className=" flex justify-center ">
-        <div className="flex flex-row items-center  border  border-gray-800 py-1 px-2 rounded ">
+        <div className="flex flex-row items-center shadow-xl border  border-gray-800 py-1 px-2 rounded z-30 w-1/2 sm:w-full mx-2 ">
           <Link to={"/"}>
             <AiOutlineArrowLeft className="text-4xl px-1 cursor-pointer " />
           </Link>
@@ -26,15 +26,20 @@ const SearchPage = () => {
             value={searchQuery}
             placeholder="Search Books.."
             onChange={(e) => setSearchQuery(e.target.value)}
-            className=" focus:outline-none text-2xl "
+            className=" focus:outline-none text-2xl w-full
+             "
           />
         </div>
       </div>
       {searchQuery && (
-        <div className=" flex justify-center ">
-          {searchedBooks.map((book) => (
-            <BookCard {...book} />
-          ))}
+        <div className=" flex justify-center flex-row flex-wrap ">
+          {searchedBooks.length !== 0 ? (
+            searchedBooks.map((book) => <BookCard {...book} />)
+          ) : (
+            <h1 className="text-4xl font-bold text-gray-600 my-10 ">
+              Book not found!
+            </h1>
+          )}
         </div>
       )}
     </div>
